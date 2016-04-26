18cf737d7a24a21bc74a930e41bb017ac37185d2
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-04-26 01:13:01 -0400

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,57 +1,108 @@
-import java.util.HashMap;
-import java.lang.reflect.Array;
-import java.util.Arrays;
+package list;
 
-public class ArrayList<v> {
-
-	public int size =0;
-	public static int capacity=16;
+public class ArrayList<T> {
 	
-	public v[] arr; 
-
-	public ArrayList(){arr=(v[])new Object[capacity];}
-	@SuppressWarnings("unchecked")
-	public ArrayList(int capacity){
-		this.capacity=capacity;
-		arr=(v[])new Object[capacity];
+	private static int CAPACITY = 10;
+	private T[] arr = (T[]) new Object[CAPACITY];
+	private int size = 0;
+	
+	public ArrayList() { }
+	
+	public int size() {
+		return size;
 	}
-	
-	public void add(v value)
-	{
-		if(size==capacity) resize();
-		size++;
-		arr[size]=value;
+	public boolean isEmpty() {
+		return size==0;
 	}
-	
-	@SuppressWarnings("unchecked")
-	public void resize(){
-		
-		 capacity=capacity*2;
-		v[] newArr=arr;
-		arr=(v[])new Object[capacity];
-		for(int i=0;i<newArr.length;i++)
-		{
-			arr[i]=newArr[i];
+	public void add(T item) {
+		if(size==CAPACITY) resize();
+		arr[size++] = item;
+	}
+	private boolean resize() {
+		int temp = CAPACITY;
+		CAPACITY = CAPACITY<<1;
+		T[] arrCopy = arr;
+		arr = (T[]) new Object[CAPACITY];
+		for(int i=0; i<temp; i++) {
+			arr[i] = arrCopy[i];
 		}
+		return true;
 	}
 	
-	public v remove(int index)
-	{
-		if (index<0||index>capacity) return null;
-		v res=arr[index];
-		for(int i=index;i<=size-1;i++)
-		{
-			arr[i]=arr[i+1];
+	public T remove(int index) {
+		if(index<0 || index>=size) throw new ArrayIndexOutOfBoundsException();
+		T res = arr[index];
+		for(int i=index; i<size-1; i++) {
+			arr[i] = arr[i+1];
 		}
 		--size;
 		return res;
 	}
 	
-	public static void main(String[] args)
-	{
-		
+	public T get(int index) {
+		if(index<0 || index>=CAPACITY) throw new ArrayIndexOutOfBoundsException();
+		return arr[index];
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<size; i++) {
+			sb.append(arr[i] + ",");
+		}
+		return sb.toString().substring(0, sb.length()-1);
+	}
 	
-	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<>();
+
+		System.out.println("add(0)");
+		list.add(0);
+		System.out.println("add(10)");
+		list.add(10);
+		System.out.println("add(20)");
+		list.add(20);
+		System.out.println("add(30)");
+		list.add(30);
+		System.out.println("add(40)");
+		list.add(40);
+		System.out.println("add(50)");
+		list.add(50);
+
+		System.out.println("items: " + list);
+		System.out.println("size: " + list.size());
+		System.out.println("get(0): " + list.get(0));
+		System.out.println("get(1): " + list.get(1));
+		System.out.println("get(2): " + list.get(2));
+		System.out.println("get(5): " + list.get(5));
+
+		System.out.println();
+		
+		System.out.println("remove(3): " + list.remove(3));
+		System.out.println("items: " + list);
+		System.out.println("size: " + list.size());
+
+		System.out.println();
+		
+		System.out.println("remove(3): " + list.remove(3));
+		System.out.println("items: " + list);
+		System.out.println("size: " + list.size());
+
+		System.out.println();
+		
+
+		System.out.println("remove(3): " + list.remove(3));
+		System.out.println("items: " + list);
+		System.out.println("size: " + list.size());
+
+		System.out.println();
+
+		System.out.println("add(60)");
+		list.add(60);
+		System.out.println("add(70)");
+		list.add(70);
+
+		System.out.println("items: " + list);
+		System.out.println("size: " + list.size());
+	}
 }
