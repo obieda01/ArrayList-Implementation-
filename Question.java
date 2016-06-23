1c7b3264ec4278aba683280155e7a3167ca6e2df
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-06-23 02:56:44 -0400

    Add files via upload

--- Question.java
+++ Question.java
@@ -1,22 +1,32 @@
-package Q15_04_Deadlock_Free_Class;
+package Q7_04_Parking_Lot;
+
+import CtCILibrary.AssortedMethods;
 
 public class Question {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		int[] res1 = {1, 2, 3, 4};
-		int[] res2 = {1, 5, 4, 1};
-		int[] res3 = {1, 4, 5};
+		ParkingLot lot = new ParkingLot();
 		
-		LockFactory.initialize(10);
-		
-		LockFactory lf = LockFactory.getInstance();
-		System.out.println(lf.declare(1, res1));
-		System.out.println(lf.declare(2, res2));
-		System.out.println(lf.declare(3, res3));	
-		
-		System.out.println(lf.getLock(1, 1));
-		System.out.println(lf.getLock(1, 2));
-		System.out.println(lf.getLock(2, 4));
+		Vehicle v = null;
+		while (v == null || lot.parkVehicle(v)) {
+			lot.print();
+			int r = AssortedMethods.randomIntInRange(0, 10);
+			if (r < 2) {
+				v = new Bus();
+			} else if (r < 4) {
+				v = new Motorcycle();
+			} else {
+				v = new Car();
+			}
+			System.out.print("\nParking a ");
+			v.print();
+			System.out.println("");
+		}
+		System.out.println("Parking Failed. Final state: ");
+		lot.print();
 	}
 
 }
